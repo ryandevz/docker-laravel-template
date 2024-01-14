64deac399170fbbf5d850555e74e9d4fc5d35079
Ryan Thomas <ryandev@protonmail.com>
2024-01-15 00:57:12 +0600

    Script fixes

--- new-project.sh
+++ new-project.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # Create laravel project
-docker-compose run --rm composer create-project laravel/laravel project
+docker compose run --rm composer create-project laravel/laravel project
 
 # Move to root folder
-mv /project/* /
+mv /project/* ./   # your current approach
+mv /project/.* ./  # this one for hidden files
+
 # Delete folder and sh file.
-rmdir /project
+rmdir project
 
 # Create messages success/fail
 if test -d /project; then
