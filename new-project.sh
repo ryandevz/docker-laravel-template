2e0b52eac6042c979ee848e57553f7d78810cf8b
Ryan Thomas <ryandev@protonmail.com>
2024-01-15 00:59:01 +0600

    Typo fix

--- new-project.sh
+++ new-project.sh
@@ -3,13 +3,13 @@
 docker compose run --rm composer create-project laravel/laravel project
 
 # Move to root folder
-mv /project/* ./   # your current approach
-mv /project/.* ./  # this one for hidden files
+mv project/* ./   # your current approach
+mv project/.* ./  # this one for hidden files
 
 # Delete folder and sh file.
 rmdir project
 
 # Create messages success/fail
-if test -d /project; then
+if test -d project; then
   echo "Directory deleted, everything works"
 fi
